35a8a816f7fdb27551e98955d1b20963c2723383
root <Cole.Kelsey@atlasschool.com>
2024-05-18 07:09:41 +0000

    edited 6.1

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,6 +9,7 @@ int main(void)
 	printf("The size of a char: %lu byte(s)\n", sizeof(char));
 	printf("The size of an int: %lu byte(s)\n", sizeof(int));
 	printf("The size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("The size of a long long int: %lu bytes(s)\n", sizeof(long long int));
 	printf("The size of a float: %lu byte(s)\n", sizeof(float));
 	printf("The size of a double: %lu byte(s)\n", sizeof(double));
 	printf("The size of a long double: %lu byte(s)\n", sizeof(long double));

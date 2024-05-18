5f06a76574ff7637ffe6721b53efc0ef0930cee9
root <ColeBiefrey@atlasschool.com>
2024-05-18 08:12:31 +0000

    rewrite 3 on 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,17 @@
  */
 int main(void)
 {
-	printf("The size of a char: %lu byte(s)\n", sizeof(char));
-	printf("The size of an int: %lu byte(s)\n", sizeof(int));
-	printf("The size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("The size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("The size of a float: %lu byte(s)\n", sizeof(float));
+	char charType;
+	int intType;
+	long int longType;
+	long long int longlongType;
+	float floatType;
+
+	printf("The size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("The size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("The size of a long int: %zu byte(s)\n", sizeof(longType));
+	printf("The size of a long long int: %zu byte(s)\n", sizeof(longlongType));
+	printf("The size of a float: %zu byte(s)\n", sizeof(floatType));
+
 	return (0);
 }

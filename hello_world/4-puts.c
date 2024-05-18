2683284804d4054ede65585ba249ce6773d61314
root <Cole.Kelsey@atlasschool.com>
2024-05-18 06:12:20 +0000

    created 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+/* This C program is intended to print an exact text string using specific functions */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

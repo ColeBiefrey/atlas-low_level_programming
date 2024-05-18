da59092ff0462e9e070a1791332f1e62bca0702d
root <Cole.Kelsey@atlasschool.com>
2024-05-18 06:30:31 +0000

    edited betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
-/* This C program is intended to print an exact text string using specifics */
+/* This C program is intended to print an exact text string using specific */
+/* main - Entry Point */
+/* Return: Always 0 (Success) */
 
 int main(void)
 {

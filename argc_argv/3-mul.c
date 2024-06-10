23e13c080957c066758c807fe6670228e4b91a4e
root <Cole.Kelsey@atlasschool.com>
2024-06-10 16:10:13 +0000

    push 3-mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply two num
+ * @argc: num of arguments
+ * @argv: array that contains arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+	if (argc !=3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
+

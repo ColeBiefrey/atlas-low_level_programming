9a0487afdf9af10163b70551b3be9f893d2df8fe
Cole Kelsey <Cole,Kelsey@atlasschool.com>
2024-06-23 05:50:24 -0500

    betty is giving the weirdest errors

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,32 +1,32 @@
-	#ifndef _DOG_H_
-	#define _DOG_H_
+#ifndef _DOG_H_
+#define _DOG_H_
 
-	/**
-	 * struct dog - struct that stores information of a dog
-	 * @name: name of dog
-	 * @age: age of dog
-	 * @owner: owner of dog
-	 * Description: struct called "dog" that stores its name, age
-	 * and name of owner
-	 */
+/**
+ * struct dog - struct that stores information of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: struct called "dog" that stores its name, age
+ * and name of owner
+ */
 
-	struct dog
+struct dog 
 
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
+{
+    char *name;
+    float age;
+    char *owner;
+}
 
-	/**
+/**
  * dog_t - Typedef for struct dog
  */
 
-	typedef struct dog dog_t;
+typedef struct dog dog_t;
 
-	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
+void init_dog(struct dog* d, char* name, float age, char* owner);
+void print_dog(struct dog* d);
+dog_t* new_dog(char* name, float age, char* owner);
+void free_dog(dog_t* d);
 
-	#endif
+#endif

343e8810a341cc9dd763d58c418b56fce40ec372
Cole Kelsey <Cole,Kelsey@atlasschool.com>
2024-06-23 05:53:54 -0500

    betty isnt even giving me a message

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,18 +16,18 @@ typedef struct dog dog_t;
  * and name of owner
  */
 
-struct dog 
+struct dog
 
 {
-    char *name;
+    char* name;
     float age;
-    char *owner;
-}
+    char* owner;
+};
 
 
-void init_dog(struct dog* d, char* name, float age, char* owner);
-void print_dog(struct dog* d);
-dog_t* new_dog(char* name, float age, char* owner);
-void free_dog(dog_t* d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif

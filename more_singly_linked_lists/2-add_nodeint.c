d91d6aece887218c9375686ac5faadf5b0332966
ColeBiefrey <Cole.Kelsey@atlasschool.com>
2024-07-07 23:15:17 -0500

    accidental ;

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -8,7 +8,7 @@
  * Return: address of new element. NUll if failed
  */
 
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t* new;
 

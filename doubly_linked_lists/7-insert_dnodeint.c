07485303b0b58f31c1b126347d6447cdd79ac409
ColeBiefrey <Cole.Kelsey@atlasschool.com>
2024-07-15 23:32:36 -0500

    error was literally cause my tired butt put the wrong freaking prototype

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
  * Return: address of new node, or NULL if fail
  */
 
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
 	dlistint_t *head;

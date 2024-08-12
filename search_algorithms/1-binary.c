326af30e36ec7dd524051f9ce97f7daf94c050da
ColeBiefrey <Cole.Kelsey@atlasschool.com>
2024-08-12 04:05:12 -0500

    addendum

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -37,3 +37,24 @@ int recursive_search(int *array, size_t size, int value)
 
 	return (recursive_search(array + half, size - half, value) + half);
 }
+
+/**
+ * binary_search - calls to binary_search to return
+ * index of num
+ * @array: input array
+ * @size: size of array
+ * @value: value to search in
+ * Return: index of num
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int index;
+
+	index = recursive_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
+		return (-1);
+
+	return (index);
+}

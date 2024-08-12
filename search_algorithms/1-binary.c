44ed369a358baf8396b3d6d668e495250771d37f
ColeBiefrey <Cole.Kelsey@atlasschool.com>
2024-08-12 04:02:14 -0500

    1-bin

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+
+/**
+ * recursive_search - searches for value in array of
+ * ints using binary search algorithm
+ * @array: input array
+ * @size: size of  array
+ * @value: value to search in
+ * Return: index of number
+ */
+
+int recursive_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+		return ((int)half);
+
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
+}
